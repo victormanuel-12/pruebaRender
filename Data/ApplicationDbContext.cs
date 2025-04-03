6d5ac113bd9b82e3f1c26cb9fc591160dd58917c
victor-manuel <178842459+victormanuel-12@users.noreply.github.com>
2025-04-03 17:58:38 -0500

    Agregar migración para crear tablas de Categorías, Usuarios, Pedidos, Productos, Pagos y DetallesPedidos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,37 +15,5 @@ public class ApplicationDbContext : IdentityDbContext
   public DbSet<DetallePedido> DetallesPedidos { get; set; }
   public DbSet<Categoria> Categorias { get; set; }
   public DbSet<Pago> Pagos { get; set; }
-      protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Configurar clave primaria compuesta para DetallePedido
-            modelBuilder.Entity<DetallePedido>()
-                .HasKey(d => new { d.IDPedido, d.IDProducto });
-
-            // Restricciones
-            modelBuilder.Entity<Usuario>()
-                .HasIndex(u => u.Email)
-                .IsUnique();
-
-            modelBuilder.Entity<Categoria>()
-                .HasIndex(c => c.Nombre)
-                .IsUnique();
-
-            modelBuilder.Entity<Pedido>()
-                .Property(p => p.FechaPedido)
-                .HasDefaultValueSql("GETDATE()");
-
-            modelBuilder.Entity<Pago>()
-                .Property(p => p.FechaPago)
-                .HasDefaultValueSql("GETDATE()");
-
-            
-
-            
-
-            
-
-            modelBuilder.Entity<Pago>()
-                .HasIndex(p => p.IDPedido)
-                .IsUnique();
-        }
+      
 }
