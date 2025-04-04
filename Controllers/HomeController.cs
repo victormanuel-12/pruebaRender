169601a0fd34c04f72f58509af2b3dede4b158db
P3dito <palominozaratep@gmail.com>
2025-04-04 00:04:27 -0500

    Creación del Home, css  y Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,43 @@
-using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
 using proyectoTienda.Models;
+using proyectoTienda.Data;
+using System.Diagnostics;
 
-namespace proyectoTienda.Controllers;
-
-public class HomeController : Controller
+namespace proyectoTienda.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            // Consulta los últimos 4 productos de categoría Jeans (2) o Polos (1)
+            var productosRecientes = _context.Productos
+                .Where(p => p.IDCategoria == 1 || p.IDCategoria == 2)
+                .OrderByDescending(p => p.IDProducto)
+                .Take(4)
+                .ToList();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(productosRecientes);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
