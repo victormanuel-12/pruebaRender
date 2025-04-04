d705b9a1d4d021ccc785a635f8902ec9e4ceb711
nick0q <nickquezada5@gmail.com>
2025-04-03 19:50:54 -0500

    agregando el detalle de producto

--- Data/Migrations/20250404004022_ProductoMigracionFinal.Designer.cs
+++ Data/Migrations/20250404004022_ProductoMigracionFinal.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using proyectoTienda.Data;
 
@@ -10,9 +11,11 @@ using proyectoTienda.Data;
 namespace proyectoTienda.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250404004022_ProductoMigracionFinal")]
+    partial class ProductoMigracionFinal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.13");
